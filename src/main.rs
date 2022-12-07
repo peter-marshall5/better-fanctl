eba74c77467ac5dd3f4c354d1467a2e4f0947731
Peter Marshall <petmshall@gmail.com>
2022-12-06 21:14:46 -0500

    Fix all warnings

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,57 @@
-use std::io::{Write, Seek, SeekFrom, Result as IoResult};
+use std::io::{Write, Seek, SeekFrom};
 use signal_hook::{consts::SIGINT, iterator::Signals};
-use std::{error::Error, thread, time::Duration, process, env, fs, cmp, fs::File};
+use std::{thread, time::Duration, process, env, fs, fs::File};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let _args: Vec<String> = env::args().collect();
 
-    let M: f32 = 0.05;
-    let BASE_SPEED: f32 = 42.0;
+    static BASE_SPEED_FALLOFF: f32 = 0.05;
+    static BASE_SPEED: f32 = 42.0;
 
-    let P: f32 = 2.0;
-    let I: f32 = 0.028;
-    let D: f32 = 0.98;
+    static ERROR_GAIN: f32 = 2.0;
+    static ACCUM_GAIN: f32 = 0.028;
+    static DERIVATIVE_GAIN: f32 = 0.98;
 
-    let A_MAX: f32 = 1000.0;
-    let A_MIN: f32 = -500.0;
+    static ACCUM_MAX: f32 = 1000.0;
+    static ACCUM_MIN: f32 = -500.0;
 
-    let TEMP_SETPOINT: f32 = 78.0;
-    let LOW_TEMP: f32 = 45.0;
+    static TEMP_SETPOINT: f32 = 78.0;
+    static LOW_TEMP: f32 = 45.0;
 
-    static MANUAL_CONTROL_ADDR: u16 = 21;
-    static SPEED_CONTROL_ADDR: u16 = 25;
-    static SPEED_MAX: u8 = 59;
-    static SPEED_MIN: u8 = 0;
-    static SPEED_DIFF: u8 = SPEED_MAX - SPEED_MIN;
-    let LOW_SPEED: u8 = SPEED_DIFF / 2 + SPEED_MIN;
+    static MANUAL_ENABLE_ADDRESS: u16 = 21;
+    static SPEED_CONTROL_ADDRESS: u16 = 25;
+    static MAX_SPEED: u8 = 59;
+    static MIN_SPEED: u8 = 0;
+    static SPEED_DIFFERENCE: u8 = MAX_SPEED - MIN_SPEED;
 
-    let LOOP_INTERVAL: u64 = 200;
-    let LOOP_DELTA: f32 = LOOP_INTERVAL as f32 / 1000.0;
-    let LOOP_INTERVAL_DIVIDED: f32 = LOOP_INTERVAL as f32 / 1000.0;
+    static LOOP_INTERVAL_MS: u64 = 200;
 
-    let SLOW_RAMP_RATE: u8 = 6;
-    let RAMP_RATE: u8 = 1;
-
-    static EC_FILE: &str = "/dev/ec";
-    static CPU_TEMP_FILE: &str = "/sys/class/hwmon/hwmon3/temp1_input";
+    static EC_PATH: &str = "/dev/ec";
+    static CPU_TEMP_PATH: &str = "/sys/class/hwmon/hwmon3/temp1_input";
 
     fn pct_to_speed(val: u8) -> u8 {
-        (val as u32 * SPEED_DIFF as u32 / 255 - SPEED_MIN as u32) as u8
+        (val as u32 * SPEED_DIFFERENCE as u32 / 255 - MIN_SPEED as u32) as u8
     }
 
     fn enable_manual_control() {
-        write_ec(MANUAL_CONTROL_ADDR, 1);
+        write_ec(MANUAL_ENABLE_ADDRESS, 1);
     }
 
     fn disable_manual_control() {
-        write_ec(MANUAL_CONTROL_ADDR, 0);
+        write_ec(MANUAL_ENABLE_ADDRESS, 0);
     }
 
     fn read_temp() -> f32 {
-        let raw: i32 = fs::read_to_string(CPU_TEMP_FILE).unwrap().trim().parse().unwrap();
+        let raw: i32 = fs::read_to_string(CPU_TEMP_PATH).unwrap().trim().parse().unwrap();
         raw as f32 / 1000.0
     }
 
     fn write_speed(speed: u8) {
-        write_ec(SPEED_CONTROL_ADDR, speed);
+        write_ec(SPEED_CONTROL_ADDRESS, speed);
     }
 
     fn write_ec(addr: u16, val: u8) {
-        let mut ec_file = File::create(EC_FILE).unwrap();
+        let mut ec_file = File::create(EC_PATH).unwrap();
 
         ec_file.seek(SeekFrom::Start(addr as u64)).unwrap();
         ec_file.write(&[val]).unwrap();
@@ -66,16 +60,16 @@ fn main() {
     let mut signals = Signals::new(&[SIGINT]).unwrap();
 
     thread::spawn(move || {
-        for sig in signals.forever() {
+        for _sig in signals.forever() {
             // println!("Received signal {:?}", sig);
             disable_manual_control();
             process::exit(0);
         }
     });
 
-    let interval = Duration::from_millis(LOOP_INTERVAL);
+    let interval = Duration::from_millis(LOOP_INTERVAL_MS);
 
-    let mut speed = SPEED_MIN;
+    let mut speed;
 
     let mut accumulation: f32 = 0.0;
     let mut last_pid_output: f32 = 0.0;
@@ -83,8 +77,6 @@ fn main() {
     let mut curr_tick: u8 = 0;
 
     enable_manual_control();
-
-    speed = pct_to_speed(255);
 
     loop {
 
@@ -94,16 +86,16 @@ fn main() {
         let error = temp - TEMP_SETPOINT;
 
         accumulation = accumulation + error;
-        if accumulation > A_MAX {
-            accumulation = A_MAX;
-        } else if accumulation < A_MIN {
-            accumulation = A_MIN;
+        if accumulation > ACCUM_MAX {
+            accumulation = ACCUM_MAX;
+        } else if accumulation < ACCUM_MIN {
+            accumulation = ACCUM_MIN;
         }
 
 
         let pi_output = {
             if temp > TEMP_SETPOINT {
-                error * P + accumulation * I
+                error * ERROR_GAIN + accumulation * ACCUM_GAIN
             } else {
                 0.0
             }
@@ -115,13 +107,13 @@ fn main() {
             if temp <= LOW_TEMP {
                 0.00
             } else if temp <= TEMP_SETPOINT {
-                BASE_SPEED + (error * M)
+                BASE_SPEED + (error * BASE_SPEED_FALLOFF)
             } else {
                 BASE_SPEED
             }
         };
 
-        let pid_output = pi_output + derivative * D;
+        let pid_output = pi_output + derivative * DERIVATIVE_GAIN;
         last_pid_output = pid_output;
         let final_output = pid_output + speed_offset;
 
