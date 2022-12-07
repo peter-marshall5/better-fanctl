887afe8c89ddef9d539e66e5dc6ab4868ebb5199
Peter Marshall <petmshall@gmail.com>
2022-12-06 22:41:57 -0500

    Add better thermal throttle prevention

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@ use std::{thread, time::Duration, process, env, fs, fs::File};
 fn main() {
     let _args: Vec<String> = env::args().collect();
 
-    static BASE_SPEED_FALLOFF: f32 = 0.05;
-    static BASE_SPEED: f32 = 42.0;
+    static BASE_SPEED: f32 = 36.0;
+    static BASE_SPEED_FALLOFF_AC: f32 = 0.2;
+    static BASE_SPEED_FALLOFF_BAT: f32 = 0.4;
 
     static ERROR_GAIN: f32 = 2.0;
     static ACCUM_GAIN: f32 = 0.028;
@@ -28,6 +29,20 @@ fn main() {
 
     static EC_PATH: &str = "/dev/ec";
     static CPU_TEMP_PATH: &str = "/sys/class/hwmon/hwmon3/temp1_input";
+    static AC_PLUGGED_PATH: &str = "/sys/class/power_supply/ACAD/online";
+
+    let interval = Duration::from_millis(LOOP_INTERVAL_MS);
+
+    let mut speed;
+
+    let mut accumulation: f32 = 0.0;
+    let mut longterm_accumulation: f32 = 0.0;
+    let mut last_pid_output: f32 = 0.0;
+
+    let mut curr_tick: u8 = 0;
+
+    let mut battery_mode: bool = false;
+
 
     fn pct_to_speed(val: u8) -> u8 {
         (val as u32 * SPEED_DIFFERENCE as u32 / 255 - MIN_SPEED as u32) as u8
@@ -67,15 +82,6 @@ fn main() {
         }
     });
 
-    let interval = Duration::from_millis(LOOP_INTERVAL_MS);
-
-    let mut speed;
-
-    let mut accumulation: f32 = 0.0;
-    let mut last_pid_output: f32 = 0.0;
-
-    let mut curr_tick: u8 = 0;
-
     enable_manual_control();
 
     loop {
@@ -92,6 +98,19 @@ fn main() {
             accumulation = ACCUM_MIN;
         }
 
+        // Moving average of temperature
+        // Tries to factor in the effects of the saturation of the cooling system
+        longterm_accumulation = {
+            if battery_mode {
+                error
+            } else {
+                if error > longterm_accumulation {
+                    error
+                } else {
+                    longterm_accumulation * 0.99 + error * 0.01
+                }
+            }
+        };
 
         let pi_output = {
             if temp > TEMP_SETPOINT {
@@ -104,10 +123,14 @@ fn main() {
         let derivative = last_pid_output - pi_output;
 
         let speed_offset = {
-            if temp <= LOW_TEMP {
+            if temp <= LOW_TEMP && battery_mode {
                 0.00
             } else if temp <= TEMP_SETPOINT {
-                BASE_SPEED + (error * BASE_SPEED_FALLOFF)
+                if battery_mode {
+                    BASE_SPEED + (longterm_accumulation * BASE_SPEED_FALLOFF_BAT)
+                } else {
+                    BASE_SPEED + (longterm_accumulation * BASE_SPEED_FALLOFF_AC)
+                }
             } else {
                 BASE_SPEED
             }
@@ -128,7 +151,7 @@ fn main() {
         };
 
         // Output for tuning the PID gains
-        // println!("T:{temp} E:{error} A:{accumulation} D:{derivative} OF:{speed_offset} O1:{pi_output} O2:{pid_output}");
+        // println!("S:{speed} T:{temp} E:{error} A:{accumulation} D:{derivative} OF:{speed_offset} O1:{pi_output} O2:{pid_output} L:{longterm_accumulation}");
 
         write_speed(speed);
 
@@ -136,6 +159,7 @@ fn main() {
 
         if curr_tick > 20 {
             enable_manual_control();
+            battery_mode = fs::read_to_string(AC_PLUGGED_PATH).unwrap().trim() == "0";
             curr_tick = 0;
         }
 
